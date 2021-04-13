0b71d5bd76f48051fd261bbf40955979189f8306
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-04-14 06:23:49 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// パッケージ群を読み込む
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// ポート番号を指定する
+var port = process.env.PORT || 3000;
+
+app.use(express.static('static'));
+
+// クライアント側のリクエストに対して、画面(htmlファイル)を返す
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/sound.html');
+});
+
+// httpサーバーを立てる
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+io.on('connection', function(socket){
+  // クライアント側にボタンが押されたこと(spk=true)送る（emit）
+  socket.on('c2s-spk', function(spk) {
+      io.emit('s2c-spk', spk);
+  });
+
+});
